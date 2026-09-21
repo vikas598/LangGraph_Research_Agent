e6e157ca6513e3af15898dc87f881969c5670300
vikas <princechat2006@gmail.com>
2026-09-21 18:37:03 +0530

    feat: analyst and perspective objects defined

---
+++ src/utils/objects.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+#creating analyst object
+class Analyst(BaseModel):
+    affiliation:str = Field(description="Primary affiliation of the Analyst")
+    name:str = Field(description="Name of the analyst.")
+    role:str = Field(description="Role of the analyst in context of the topic")
+    description:str = Field(description="Description of the analyst focus, concern and motives")
+
+    @property
+    def persona(self)->str:
+        return f"Name : {self.name}\n Role: {self.role} \n Affiliation: {self.affiliation} \n Description: {self.description}"
+
+class Perspective(BaseModel):
+    analysts : List[Analyst] = Field(description=" Comprehensive list of analysts with their role, name, affiliation and description")
